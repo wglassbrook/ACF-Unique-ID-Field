32f5f5a8793b94da916a0e0fd97b9568288cd3dd
Wayne Glassbrook <3577550+wglassbrook@users.noreply.github.com>
2024-10-22 12:09:33 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php 
+/**
+ *  @package   		Advanced Custom Fields Unique ID Field
+ *  @author    		Wayne Glassbrook <wayne@designly.net>
+ *  @license   		GPL-2.0+
+ *  @link      		http://www.designly.net
+ *  @copyright 		2023 Wayne Glassbrook
+ *
+ *	Plugin Name:	Advanced Custom Fields Unique ID Field
+ *	Plugin URI: 	http://www.designly.net
+ *	Description:	Field that auto generates a Unique ID
+ *	Version:		  1.0.0
+ *	Author:			  Wayne Glassbrook
+ *	Author URI:		http://www.designly.net
+ *	License:		  GPLv2
+ *  License URI:  http://www.gnu.org/licenses/gpl-2.0.txt
+ *  
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+};
+
+// Include Unique IDs Field
+include('inc/_uniqueID.php');
+
+/*
+	Copyright 2023 Wayne Glassbrook (email : wayne@designly.net)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+?>
