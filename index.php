8d8929c550492fa760548f34b5dbf9aca995510c
Wayne Glassbrook <3577550+wglassbrook@users.noreply.github.com>
2024-10-22 12:14:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php 
 /**
  *  @package   		Advanced Custom Fields Unique ID Field
- *  @author    		Wayne Glassbrook <wayne@designly.net>
+ *  @author    		Wayne Glassbrook <wayne@ciesadesign.com>
  *  @license   		GPL-2.0+
- *  @link      		http://www.designly.net
- *  @copyright 		2023 Wayne Glassbrook
+ *  @link      		https://ciesadesign.com
+ *  @copyright 		2023 Wayne Glassbrook/Ciesa Inc.
  *
- *	Plugin Name:	Advanced Custom Fields Unique ID Field
- *	Plugin URI: 	http://www.designly.net
- *	Description:	Field that auto generates a Unique ID
- *	Version:		  1.0.0
- *	Author:			  Wayne Glassbrook
- *	Author URI:		http://www.designly.net
- *	License:		  GPLv2
+ *  Plugin Name:	Advanced Custom Fields Unique ID Field
+ *  Plugin URI: 	https://ciesadesign.com
+ *  Description:	Field that auto generates a Unique ID
+ *  Version:		  1.0.0
+ *  Author:			  Wayne Glassbrook
+ *  Author URI:		https://ciesadesign.com
+ *  License:		  GPLv2
  *  License URI:  http://www.gnu.org/licenses/gpl-2.0.txt
  *  
- */
+**/
 
 // If this file is called directly, abort.
 if ( ! defined( 'WPINC' ) ) {
@@ -25,14 +25,14 @@ if ( ! defined( 'WPINC' ) ) {
 // Include Unique IDs Field
 include('inc/_uniqueID.php');
 
-/*
-	Copyright 2023 Wayne Glassbrook (email : wayne@designly.net)
-
+/**
+	Copyright 2023 Wayne Glassbrook/Ciesa Inc. (email : wayne@ciesadesign.com)
+	
 	This program is free software; you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
 	the Free Software Foundation; either version 2 of the License, or
 	(at your option) any later version.
-
+	
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
@@ -40,5 +40,5 @@ include('inc/_uniqueID.php');
 	You should have received a copy of the GNU General Public License
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-*/
+**/
 ?>
